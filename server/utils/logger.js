f342cf12d2a12770aa5b13208e114a6841d9d595
Tony <atony.mtz@gmail.com>
2020-03-24 19:09:22 -0700

    adding rollbar logging

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -1,34 +1,10 @@
-const winston = require('winston');
+const Rollbar = require("rollbar");
 
-const debugMode = process.env.NODE_ENV === 'development';
-
-const errorStackFormat = winston.format((info) => {
-  if (info instanceof Error) {
-    return {
-      ...info,
-      stack: info.stack,
-      message: info.message,
-    };
-  }
-  return info;
+const tokenRollbar = process.env['TOKEN_ROLLBAR'];
+const rollbar = new Rollbar({
+  accessToken: tokenRollbar,
+  captureUncaught: true,
+  captureUnhandledRejections: true
 });
 
-const logger = winston.createLogger({
-  levels: winston.config.syslog.levels,
-  format: winston.format.combine(
-    winston.format.colorize({ all: true }),
-    winston.format.align(),
-    winston.format.timestamp(),
-    winston.format.splat(), // Allows the text to be formatted at log time
-    winston.format.simple(), // Same as above
-    errorStackFormat(),
-    winston.format.printf((info) => `[${info.timestamp}] ${info.level}: ${info.message} ${(info.payload && `\n${JSON.stringify(info.payload)}`) || ''} ${info.stack ? `\n${info.stack}` : ''}`),
-  ),
-  transports: [
-    new winston.transports.Console({
-      level: (debugMode ? 'debug' : 'info'),
-    }),
-  ],
-});
-
-module.exports = logger;
+module.exports = rollbar;
